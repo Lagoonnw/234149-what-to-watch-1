a8f3e0702d3aff4ee8702299036cfc904e45ef35
Ekaterina Krutakova <lagoonnw@gmail.com>
2019-05-04 22:11:23 +0500

    Вносит правки после замечаний

--- src/components/main-screen/main-screen.test.js
+++ src/components/main-screen/main-screen.test.js
@@ -5,7 +5,7 @@ import {MainScreen} from './main-screen.jsx';
 const moviesList = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Snatch`];
 
 describe(`Main screen should render correctly`, () => {
-  test(`Should render main screen with 4 movies`, () => {
+  test(`Should render main-screen with moviesList length`, () => {
     const tree = renderer.create(<MainScreen moviesList={moviesList}/>).toJSON();
     expect(tree).toMatchSnapshot();
   });
