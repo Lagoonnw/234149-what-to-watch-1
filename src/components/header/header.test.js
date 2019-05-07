e2940c907974312e22170e6e339a7d2790889521
ekrutakova <e.krutakova@driventa.com>
2019-05-07 08:32:02 +0500

    Выносит шапку в отдельный компонент

---
+++ src/components/header/header.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {Header} from "./header.jsx";
+
+describe(`Header should render`, () => {
+  test(`Should render header component correctly`, () => {
+    const tree = renderer.create(<Header/>);
+    expect(tree).toMatchSnapshot();
+  });
+});
--- src/components/main-screen/main-screen.jsx
+++ src/components/main-screen/main-screen.jsx
@@ -1,5 +1,6 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
+import {Header} from "../header/header.jsx";
 
 export const MainScreen = (props) => {
   const {moviesList, onMovieCardClick} = props;
@@ -44,21 +45,7 @@ export const MainScreen = (props) => {
 
         <h1 className="visually-hidden">WTW</h1>
 
-        <header className="page-header movie-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <div className="user-block">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-            </div>
-          </div>
-        </header>
+        <Header/>
 
         <div className="movie-card__wrap">
           <div className="movie-card__info">
