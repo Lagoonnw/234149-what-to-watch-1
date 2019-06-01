909cd42798cb942c77e3023d2f3fbc139b49cc7d
ekrutakova <e.krutakova@driventa.com>
2019-06-01 14:39:32 +0500

    Поправила валидатор

--- src/helpers/validators/validators.js
+++ src/helpers/validators/validators.js
@@ -1,5 +1,5 @@
 export const validator = {
   email: (value) => (value.includes(`@`)),
-  required: (value) => (value) ? value.length > 1 : false
+  required: (value) => value && value.length > 0
 };
 
